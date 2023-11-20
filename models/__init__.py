b3037d68a5432198b52a8754530ff5e340fb08b4
e-funes <edu.centurion.funes@gmail.com>
2023-11-20 16:02:50 -0300

    More silent mode

--- models/__init__.py
+++ models/__init__.py
@@ -31,7 +31,7 @@ def get_cell_based_tiny_net(config):
         config = dict2config(config, None)  # to support the argument being a dict
     super_type = getattr(config, "super_type", "basic")
     group_names = ["DARTS-V1", "DARTS-V2", "GDAS", "SETN", "ENAS", "RANDOM", "generic"]
-    print(super_type, config.name )
+    # print(super_type, config.name )
     if super_type == "basic" and config.name in group_names:
         from .cell_searchs import nas201_super_nets as nas_super_nets
 
